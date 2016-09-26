997b32d4973889d52e36283a64037f94a105d7e5
Teemu Ropilo <sebastian.source@gmail.com>
2016-09-27 01:29:40 +0300

    - created base classes - started work on basic functionality
    - plan diagram added to repo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    private static CharacterPanel _charPanelInstance;
+
+    private static UIManager _uiManagerInstance;
+
+    public static CharacterPanel CharPanelInstance
+    {
+        get
+        {
+            //Logic for creating CharacterPanel
+            if (_charPanelInstance == null)
+            {
+                GameObject go = new GameObject("Character Panel");
+                go.AddComponent<CharacterPanel>();
+            }
+
+            return _charPanelInstance;
+        }
+    }
+
+    public static UIManager UIManagerInstance
+    {
+        get
+        {
+            //Logic for creating UIManager
+            if (_uiManagerInstance == null)
+            {
+                GameObject go = new GameObject("UI Manager");
+                go.AddComponent<UIManager>();
+            }
+
+            return _uiManagerInstance;
+        }
+    }
+    public bool isAlive { get; set; }
+
+    void Awake()
+    {
+        _charPanelInstance = CharPanelInstance;
+        _uiManagerInstance = UIManagerInstance;
+    }
+
+    void Start()
+    {
+        isAlive = true;
+    }
+}
